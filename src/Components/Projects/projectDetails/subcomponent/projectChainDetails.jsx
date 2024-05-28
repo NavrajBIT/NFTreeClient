743db51e0024f7495e8212867f7875260e90ae94
Vincent Samuel <chintuvincent@gmail.com>
2024-05-28 10:36:34 +0530

    added blockchain details

--- src/Components/Projects/projectDetails/subcomponent/projectChainDetails.jsx
+++ src/Components/Projects/projectDetails/subcomponent/projectChainDetails.jsx
@@ -2,33 +2,47 @@ import React from "react";
 
 const ProjectChainDetails = ({ details }) => {
   return (
-    <div style={{ padding: "20px 90px", background: "green", color: "white" }}>
-      {/* <button onClick={() => console.log(details?.project)}>Click</button> */}
-      <div style={{ fontSize: "2rem" }}>Blockchain Details</div>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          gap: "20px",
-          wordWrap: "break-word",
-        }}
-      >
-        <div>Tx Signature : {details?.project?.blockchain?.signature}</div>
-        <div>
-          Program Account : {details?.project?.blockchain?.program_account}
+    <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
+      <h1 className="projectHeading">Blockchain Details</h1>
+      <div className="blockchainDetails" >
+        <div style={{ display: "flex", gap: "10px" }}>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              gap: "20px",
+              wordWrap: "break-word",
+            }}
+          >
+            <div style={{fontWeight:"600"}}>Tx Signature : </div>
+            <div style={{fontWeight:"600"}}>
+              Program Account :
+            </div>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              gap: "20px",
+              wordWrap: "break-word",
+            }}
+          >
+            <div> {details?.project?.blockchain?.signature}</div>
+            <div>
+              {details?.project?.blockchain?.program_account}
+            </div>
+          </div>
         </div>
         <button
+          className="progressBtn"
           onClick={() => {
             window.open(
               `https://explorer.solana.com/tx/${details?.project?.blockchain?.signature}?cluster=devnet`
             );
           }}
           style={{
-            width: "fit-content",
-            padding: "10px",
-            background: "white",
-            borderRadius: "10px",
-            color: "green",
+            margin: "auto",
+            padding: "1.3% 8%"
           }}
         >
           Explorer
